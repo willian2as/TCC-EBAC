be74fd5f3c0407828e1f4ed7aff107695156a90d
Willian Almeida <aas.willian@gmail.com>
2023-11-16 15:37:25 -0300

    Segundo commit

---
+++ cypress/e2e/carrinho.cy.js
@@ -0,0 +1,12 @@
+/// <reference types="cypress" />
+
+context ('Adicionar item ao carrinho', () => {
+    beforeEach(() => {
+        
+    });
+
+    it('Deve cadastrar tres itens ao carrinho', () => {
+        cy.visit('produtos/')
+        //cy.get('#username').type('aluno_ebac@teste.com')
+    })
+})
